8e1d9ff8c05bf9ece991d9f8278769577eecc388
german gomez <germangb42@gmail.com>
2016-03-12 23:38:01 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# --mpd
-μ-MPD, for all your MPD in statusbar/notification needs
+# μMPD
+μMPD is a very small MPD client meant for statusbars and notifications. The script will connect to the MPD server and listen
+to playlist and mixer events (new songs playin and volume changes)
+
+For each event, a new line is printed to **stdout**, so that you can pipe it to whatever program you want. This README includes examples for dzen2
+
+When the connection to MPD is lost, the script stops running.
+
+## Usage example
+```
+$ # host and port will default to localhost and 6600 respectively
+$ ./mmpd.py --host 127.0.0.1 --port 6600
+```
+
+## Usage example with dzen2
+```
+$ ./mmpd.py --host 127.0.0.1 --port 6600 | dzen2
+```
+
+## Display formats
+
+You can modify the output format. with `--format` you specify the "song playing" format. With `--format-error` you speify the format of the "connection error" message. With `--format--none` you specify the format of the message that shows up when the MPD playlist ends
+```
+$ ./mmpd.py \
+    --format 'Now playing {title} by {artist}' \
+    --format-error 'Connection to {host}:{port} refused :/' \
+    --format-none 'There is no more music'
+```
+
+Tag | Data
+--- | ---
+{title} | Song title (falls back to the file name)
+{artist} | Song artist
+{date} | Song date
+{genre} | Song genre
+{track} | Track number
+{album} | Song album
+{volume} | Volume 0-100
+{host} | Hostname from --host
+{port} | Port from --port
+
+## Random stuff
+### I want a fancy volume indicator
+You can pipe the output to some program that reformats the text
+```
+$ # cool_volume.py is a script that finds the volume integer and replaces it with something fancy like ▁▂▃▅█
+$ # the cw{volume} tag has to be wrapped as specified in this --format
+$ ./mmpd.py --format 'Now Playing {title} #({volume})' | ./cool_volume.py | dzen2
+```
+
+### I want dzen2 to only appear when something starts playing
+One option is to reformat the output as a unix command, including the pipe to dzen2 there
+```
+$ TIME=4 && ./mmpd.py --no-mixer \
+    --format 'echo "MMPD :: ♫ Now Playing ♪ {title} by {artist}" | dzen2 -p $TIME ' | sh
+```
+This will make dzen show up and disappear after a few $TIME seconds. If you go with this option, then disable mixer events.
+## Additional information
+The Music Player Daemon protocol [http://www.musicpd.org/doc/protocol/](http://www.musicpd.org/doc/protocol/)
---
+++ mmpd.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import sys
+import socket
+import argparse
+
+def send (sock, data):
+    cc = sock.send(data)
+    if cc == 0:
+        raise ConnectionError
+    return cc
+
+def recv (sock, buffsize):
+    rr = sock.recv(buffsize)
+    if len(rr) == 0:
+        raise ConnectionError
+    return rr
+
+def parse (resp):
+    parsed = {}
+    lines = resp.split('\n')
+    for line in lines:
+        data = line.split(':', maxsplit=1)
+        if len(data) == 2:
+            parsed[data[0]] = data[1].lstrip()
+    return parsed
+
+def main():
+    argp = argparse.ArgumentParser()
+    argp.add_argument('--host', default='localhost')
+    argp.add_argument('--port', type=int, default=6600)
+    argp.add_argument('--format', default='MMPD :: ♫ {title} :: {artist} :: {album} ({date}) :: volume {volume}')
+    argp.add_argument('--format-error', default='MMPD :: Connection to {host}:{port} refused')
+    argp.add_argument('--format-none', default='MMPD :: No more music to play :(')
+    argp.add_argument('--verbose', action='store_true')
+    argp.add_argument('--no-mixer', action='store_true', help='Don\'t listen to mixer events (volume updates)')
+    args = vars(argp.parse_args())
+
+    address = (args['host'], args['port'])
+    client = socket.socket()
+    
+    try:
+        client.connect(address)
+        ok = recv(client, 1024)
+        if args['verbose']:
+            sys.stderr.write(ok.decode())
+        
+        first = True
+
+        while True:
+            if not first:
+                # wait until something happens in the player
+                idle = 'idle player mixer\n'
+                if args['no_mixer']:
+                    idle = 'idle player\n'
+                send(client, idle.encode())
+                event = parse(recv(client, 1024).decode())
+                if args['verbose']:
+                    sys.stderr.write(str(event)+'\n')
+
+            first = False
+            
+            # send a currentsong request
+            send(client, b'currentsong\n')
+            song = parse(recv(client, 1024).decode()) 
+            if args['verbose']:
+                sys.stderr.write(str(song)+'\n')
+            
+            # get status
+            send(client, b'status\n')
+            status = parse(recv(client, 1024).decode())
+            if args['verbose']:
+                sys.stderr.write(str(status)+'\n')
+            
+            # write stuff
+            volume = status['volume']
+            title = song.get('Title', song['file'])
+            artist = song.get('Artist', 'Unknown')
+            album = song.get('Album', 'Unknown')
+            date = song.get('Date', 'Unknown')
+            track = song.get('Track', 'Unknown')
+           
+            if not bool(song):
+                sys.stdout.write(args['format_none'].format(host=args['host'], port=args['port'], volume=volume))
+                sys.stdout.write('\n')
+            else:
+                sys.stdout.write(args['format'].format(
+                    host=args['host'], port=args['port'],
+                    volume=volume, title=title, artist=artist, album=album, date=date)) 
+                sys.stdout.write('\n')
+            
+            sys.stdout.flush()
+
+    except ConnectionError:
+        sys.stdout.write(args['format_error'].format(host=args['host'], port=args['port']))
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        client.close()
+
+if __name__ == '__main__':
+    main()
